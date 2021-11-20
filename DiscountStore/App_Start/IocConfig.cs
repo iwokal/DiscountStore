27b41abae8aaeb0066f5135d4be83b8049935000
iwokal <kalinskiiwo@gmail.com>
2021-11-20 14:02:36 +0100

    Added Ioc container

---
+++ DiscountStore/App_Start/IocConfig.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using System.Reflection;
+using System.Web.Http;
+
+namespace DiscountStore
+{
+    public static class IocConfig
+    {
+        public static void Configure()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            var container = builder.Build();
+            var resolver = new AutofacWebApiDependencyResolver(container);
+            GlobalConfiguration.Configuration.DependencyResolver = resolver;
+        }
+    }
+}
