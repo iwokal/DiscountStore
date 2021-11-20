6c75ff7fc2eee6ab526a7fa2e5c13c5635d6cf53
iwokal <kalinskiiwo@gmail.com>
2021-11-20 15:52:10 +0100

    Added tests for cart service remove and get total

--- DiscountStore.Tests/Services/CartServiceTest.cs
+++ DiscountStore.Tests/Services/CartServiceTest.cs
@@ -3,6 +3,7 @@ using DiscountStore.Areas.Cart.Models;
 using DiscountStore.Areas.Cart.Services;
 using DiscountStore.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace DiscountStore.Tests.Controllers
@@ -33,6 +34,128 @@ namespace DiscountStore.Tests.Controllers
             Assert.IsNotNull(service.currentCart.Items);
             Assert.AreEqual(1, service.currentCart.Items.Count);
             Assert.AreEqual(item, service.currentCart.Items[0]);
+        }
+
+        [TestMethod]
+        public void Remove()
+        {
+            // Arrange
+            CartService service = new CartService();
+            Item item = new Item
+            {
+                SKU = "test",
+                Price = 1,
+                Discount = new Discount
+                {
+                    Price = 2,
+                    Quantity = 3
+                }
+            };
+
+            Item item2 = new Item
+            {
+                SKU = "test",
+                Price = 1,
+                Discount = new Discount
+                {
+                    Price = 2,
+                    Quantity = 3
+                }
+            };
+
+            service.currentCart.Items = new List<Item>() { item, item2 };
+
+            // Act 
+            service.Remove(item);
+
+            // Assert
+            Assert.IsNotNull(service.currentCart.Items);
+            Assert.AreEqual(1, service.currentCart.Items.Count);
+            Assert.IsFalse(service.currentCart.Items.Contains(item));
+        }
+
+        [TestMethod]
+        public void GetTotal()
+        {
+            // Arrange
+            CartService service = new CartService();
+            Item item = new Item
+            {
+                SKU = "test",
+                Price = 1,
+                Discount = new Discount
+                {
+                    Price = 2,
+                    Quantity = 3
+                }
+            };
+
+            Item item2 = new Item
+            {
+                SKU = "test",
+                Price = 1,
+                Discount = new Discount
+                {
+                    Price = 2,
+                    Quantity = 3
+                }
+            };
+
+            service.currentCart.Items = new List<Item>() { item, item2};
+
+            // Act 
+            var total = service.GetTotal();
+
+            // Assert
+            Assert.IsNotNull(service.currentCart.Items);
+            Assert.AreEqual(2, total);
+        }
+
+        [TestMethod]
+        public void GetTotalWithDiscount()
+        {
+            // Arrange
+            CartService service = new CartService();
+            Item mug1 = new Item
+            {
+                SKU = "mug",
+                Price = 1,
+                Discount = new Discount
+                {
+                    Price = 1.5,
+                    Quantity = 2
+                }
+            };
+
+            Item vase = new Item
+            {
+                SKU = "vase",
+                Price = 1.2,
+                Discount = new Discount
+                {
+                    Price = 2,
+                    Quantity = 3
+                }
+            };
+
+            Item mug2 = new Item
+            {
+                SKU = "mug",
+                Price = 1,
+                Discount = new Discount
+                {
+                    Price = 1.5,
+                    Quantity = 2
+                }
+            };
+            service.currentCart.Items = new List<Item>() { mug1, vase, mug2 };
+
+            // Act 
+            var total = service.GetTotal();
+
+            // Assert
+            Assert.IsNotNull(service.currentCart.Items);
+            Assert.AreEqual(2.7, total);
         }
     }
 }
