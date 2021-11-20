73f7e021e230f3526b5aacf437f91b34f6c1ab09
iwokal <kalinskiiwo@gmail.com>
2021-11-20 16:41:53 +0100

    Implement remove and get total methods

--- DiscountStore/Areas/Cart/Services/CartService.cs
+++ DiscountStore/Areas/Cart/Services/CartService.cs
@@ -1,6 +1,7 @@
 ﻿using DiscountStore.Areas.Cart.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DiscountStore.Areas.Cart.Services
 {
@@ -15,12 +16,21 @@ namespace DiscountStore.Areas.Cart.Services
 
         public void Remove(Item item)
         {
-            throw new NotImplementedException();
+            _cart.Items.Remove(item);
         }
 
         public double GetTotal()
         {
-            throw new NotImplementedException();
+            double total = 0;
+            List<string> skus = _cart.Items.Select(x=> x.SKU).Distinct().ToList();
+            foreach (var sku in skus)
+            {
+                var item = _cart.Items.FirstOrDefault(x=>x.SKU == sku);
+                var quantity = _cart.Items.Count(x => x.SKU == item.SKU);
+                var price = quantity / item.Discount.Quantity * item.Discount.Price + quantity % item.Discount.Quantity * item.Price;
+                total += price;
+            }
+            return total;
         }
     }
 }
